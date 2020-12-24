1413a53809362f454785a33bb841d380590fdb12
Tartarus <39802246+Tartarus2014@users.noreply.github.com>
2020-12-24 17:50:04 +0800

    Update ZhongQing.js

--- JS/ZhongQing.js
+++ JS/ZhongQing.js
@@ -1,3 +1,4 @@
+# 中青看点
 http-request https:\/\/\w+\.youth\.cn\/TaskCenter\/(sign|getSign) script-path=https://raw.githubusercontent.com/Sunert/Scripts/master/Task/youth.js, tag=中青看点极速版
 http-request https:\/\/ios\.baertt\.com\/v5\/article\/complete script-path=https://raw.githubusercontent.com/Sunert/Scripts/master/Task/youth.js, requires-body=true, tag=中青看点极速版
 http-request https:\/\/ios\.baertt\.com\/v5\/article\/red_packet script-path=https://raw.githubusercontent.com/Sunert/Scripts/master/Task/youth.js, requires-body=true, tag=中青看点极速版
@@ -9,6 +10,7 @@ http-request https://ios.baertt.com/v5/article/complete.json script-path=https:/
 
 cron "*/30 0-23 * * *" script-path=https://raw.githubusercontent.com/CenBoMin/QuantumultX/master/Scripts/Youth/youth.js, tag=中青看点极速版
 cron "10 1-23 * * *" script-path=https://raw.githubusercontent.com/CenBoMin/QuantumultX/master/Scripts/Youth/Youth_AutoRead.js, tag=中青阅读
+
 
 hostname = *.youth.cn, ios.baertt.com 
 
