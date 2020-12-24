9557a615ecbbcf97cc33b33eb7b93c81c3d4a8ac
Tartarus <39802246+Tartarus2014@users.noreply.github.com>
2020-12-24 17:59:25 +0800

    Update ZhongQing.js

--- JS/ZhongQing.js
+++ JS/ZhongQing.js
@@ -1,22 +1,16 @@
 # 中青看点
 # 签到cookie:
 # 进入app，进入任务中心或者签到一次
-
 # 阅读请求body&阅读时长:
 # 阅读一篇文章或者视频到获取金币奖励
-
 # 惊喜红包:
 # 在阅读文章拉下面有个惊喜红包，点击获取
 
-# 2.手动执行一次定时脚本-”中青看点极速版”,看签到是不是正常
-
-# 3.到[重写]-[引用],启动YouthGetBody禁用YouthCookie,获取文章body
-
+# 手动执行一次定时脚本-”中青看点极速版”,看签到是不是正常
 # 阅读请求body:阅读一篇文章或者视频到获取金币奖励,通知提示body1
 
-# 4.手动执行一次定时脚本-”中青阅读”,是不是运行正常
-
-# 5.获取更多的body,一天一般上限7200金币,建议获取200个body
+# 手动执行一次定时脚本-”中青阅读”,是不是运行正常
+# 获取更多的body,一天一般上限7200金币,建议获取200个body
 
 
 http-request https:\/\/\w+\.youth\.cn\/TaskCenter\/(sign|getSign) script-path=https://raw.githubusercontent.com/Sunert/Scripts/master/Task/youth.js, tag=中青看点极速版
